4c7da553529c3c1162325eaf3c21a7a0c59bb225
imaisachiyo <imaisachiyo.1030@gmail.com>
2025-02-02 17:55:54 +0900

    13.クリックされた時に処理を実行する法方

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <script setup>
   const title = 'Vue.js Course'
   let price = 9.99
+  function increment(){
+    console.log('click')
+  }
 </script>
 <template>
   <h1>Title: {{ title }}</h1>
   <h2>Price: {{ price -  1 }}</h2>
+  <button @click="increment">ボタン</button>
 </template>
 
