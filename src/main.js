d5ffaff1bbcff405693fc4e9ccee1e668d963196
imaisachiyo <imaisachiyo.1030@gmail.com>
2025-02-02 17:42:07 +0900

    12.Javascriptのデータを表示する法方

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
-import './assets/main.css'
-
-import { createApp } from 'vue'
-import App from './App.vue'
-
+import { createApp } from 'vue' // vueディレクトリからcreateAppを読み込む
+import App from './App.vue' // App.vueを読み込みJSに変換し、Appという名前をつける
+// createApp関数の引数に渡されたAPPをもとに、ユーザーインターフェイスを作る！
+// createAppで作られたUIを表示するためにmountメソッドを使う
+// mountの引数にintex.htmlのdiv要素のid値を指定
+// intex.htmlのid=APPをもつdiv要素にUIを表示する
 createApp(App).mount('#app')
